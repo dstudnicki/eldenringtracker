64d273379f609a6159fd1ab3ebf7c868ea9ac7ef
Dominik Studnicki <domistu@poczta.fm>
2024-06-23 23:59:51 +0200

    feat(bosses): add check to boss

--- src/app/bosses/page.tsx
+++ src/app/bosses/page.tsx
@@ -1,36 +1,41 @@
+"use client";
 import Image from "next/image";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
-async function getData() {
-    const response = await fetch("http://localhost:3000/api/bosses");
+export default function BossesPage() {
+    const [data, setData] = useState({ bosses: [] });
+    const [isSelected, setIsSelected] = useState(false);
 
-    if (!response.ok) {
-        // This will activate the closest `error.js` Error Boundary
-        throw new Error("Failed to fetch data");
-    }
+    const fetchData = async () => {
+        const data = await axios.get("http://localhost:3000/api/bosses");
+        setData(data.data);
+    };
 
-    return response.json();
-}
-
-export default async function BossesPage() {
-    const data = await getData();
-    console.log(data);
+    useEffect(() => {
+        fetchData();
+    }, []);
 
     return (
         <main className="flex flex-col px-4 lg:px-8 xl:px-24">
             <div className="grid sm:grid-cols-2 lg:grid-cols-5 grid-rows-1 gap-4">
-                {data.map((boss: any) => (
-                    <Card key={boss.id}>
-                        <CardHeader className="pb-2">
-                            <CardTitle className="text-md font-medium">{boss.name}</CardTitle>
-                        </CardHeader>
-                        <CardContent className="pb-0 text-2xl font-bold">
-                            {" "}
-                            {boss.image ? <Image className="rounded-md" width={300} height={300} src={boss.image} alt="Picture of boss" /> : <p>No image</p>}
-                        </CardContent>
-                        <CardContent className="text-xs text-muted-foreground">{boss.location}</CardContent>
-                    </Card>
-                ))}
+                {!isSelected ? (
+                    data.bosses.map((boss: any) => (
+                        <Card key={boss.id}>
+                            <CardHeader className="pb-2">
+                                <CardTitle className="text-md font-medium">{boss.name}</CardTitle>
+                            </CardHeader>
+                            <CardContent className="pb-0 text-2xl font-bold">
+                                {boss.image ? <Image className="rounded-md" width={300} height={300} src={boss.image} alt="Picture of boss" /> : <p>No image</p>}
+                            </CardContent>
+                            <CardContent className="text-xs text-muted-foreground">{boss.location}</CardContent>
+                            <button onClick={() => setIsSelected(!isSelected)}>Check</button>
+                        </Card>
+                    ))
+                ) : (
+                    <p>Checked</p>
+                )}
             </div>
         </main>
     );
