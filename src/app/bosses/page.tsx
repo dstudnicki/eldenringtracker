4872aae56edc81c14ca9584a27efaf8ed1057431
Dominik Studnicki <domistu@poczta.fm>
2024-06-24 00:22:18 +0200

    fix(bosses): fix check button to check only one selected boss not whole array

--- src/app/bosses/page.tsx
+++ src/app/bosses/page.tsx
@@ -4,13 +4,31 @@ import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { useEffect, useState } from "react";
 import axios from "axios";
 
+// async function getData() {
+//     const response = await fetch("http://localhost:3000/api/bosses");
+
+//     if (!response.ok) {
+//         // This will activate the closest `error.js` Error Boundary
+//         throw new Error("Failed to fetch data");
+//     }
+
+//     return response.json();
+// }
+
 export default function BossesPage() {
-    const [data, setData] = useState({ bosses: [] });
-    const [isSelected, setIsSelected] = useState(false);
+    // const data = await getData();
+    // console.log(data);
+
+    const [data, setData] = useState([]);
+    const [isSelected, setIsSelected] = useState<{ [key: string]: any }>({});
 
     const fetchData = async () => {
         const data = await axios.get("http://localhost:3000/api/bosses");
         setData(data.data);
+    };
+
+    const toggleSelected = (id: number) => {
+        setIsSelected((prevState) => ({ ...prevState, [id]: !prevState[id] }));
     };
 
     useEffect(() => {
@@ -20,8 +38,8 @@ export default function BossesPage() {
     return (
         <main className="flex flex-col px-4 lg:px-8 xl:px-24">
             <div className="grid sm:grid-cols-2 lg:grid-cols-5 grid-rows-1 gap-4">
-                {!isSelected ? (
-                    data.bosses.map((boss: any) => (
+                {data.map((boss: any) =>
+                    !isSelected[boss.id] ? (
                         <Card key={boss.id}>
                             <CardHeader className="pb-2">
                                 <CardTitle className="text-md font-medium">{boss.name}</CardTitle>
@@ -30,11 +48,9 @@ export default function BossesPage() {
                                 {boss.image ? <Image className="rounded-md" width={300} height={300} src={boss.image} alt="Picture of boss" /> : <p>No image</p>}
                             </CardContent>
                             <CardContent className="text-xs text-muted-foreground">{boss.location}</CardContent>
-                            <button onClick={() => setIsSelected(!isSelected)}>Check</button>
+                            <button onClick={() => toggleSelected(boss.id)}>Check</button>
                         </Card>
-                    ))
-                ) : (
-                    <p>Checked</p>
+                    ) : null
                 )}
             </div>
         </main>
