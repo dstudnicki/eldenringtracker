776d86953678275342276942b325f9da093e3401
Dominik Studnicki <domistu@poczta.fm>
2024-09-06 17:41:00 +0200

    fix: fix suspense

--- src/app/bosses/page.tsx
+++ src/app/bosses/page.tsx
@@ -14,6 +14,7 @@ import { cn } from "@/lib/utils";
 import axios from "axios";
 import { useSession } from "next-auth/react";
 import { useRouter, useSearchParams } from "next/navigation";
+import { Suspense } from "react";
 
 export default function BossesPage() {
     const baseURL = process.env.NEXT_PUBLIC_BASE_URL;
@@ -147,79 +148,85 @@ export default function BossesPage() {
     const displayedData = showSelectedOnly ? selectedBosses : showFilteredData();
 
     return (
-        <main className="flex flex-col px-4 sm:px-8 lg:px-12 xl:px-0 xl:container">
-            {loading ? (
-                <Skeleton className="h-9 w-[200px]" />
-            ) : (
-                <>
-                    <div className="flex gap-4">
-                        <Popover open={open} onOpenChange={setOpen}>
-                            <PopoverTrigger asChild>
-                                <Button variant="outline" role="combobox" aria-expanded={open} className="w-[200px] justify-between">
-                                    {regionValue ? locationsForFilter.find((region) => region === regionValue) : "Search region..."}
-                                    <ChevronsUpDown className="ml-2 h-4 w-4 shrink-0 opacity-50" />
-                                </Button>
-                            </PopoverTrigger>
-                            <PopoverContent className="w-[200px] p-0">
-                                <Command>
-                                    <CommandInput placeholder="Search region..." />
-                                    <CommandList>
-                                        <CommandEmpty>No location found.</CommandEmpty>
-                                        <CommandGroup>
-                                            {locationsForFilter.map((region) => (
-                                                <CommandItem key={region} value={region} onSelect={() => handleLocationSelect(region)}>
-                                                    <Check className={cn("mr-2 h-4 w-4", regionValue === region ? "opacity-100" : "opacity-0")} />
-                                                    {region}
-                                                </CommandItem>
-                                            ))}
-                                        </CommandGroup>
-                                    </CommandList>
-                                </Command>
-                            </PopoverContent>
-                        </Popover>
-                        <Button variant="outline" onClick={handleToggleView}>
-                            {showSelectedOnly ? "View All Bosses" : "View Selected Bosses"}
-                        </Button>
-                    </div>
-                    <section className="grid sm:grid-cols-2 lg:grid-cols-4 grid-rows-1 gap-4 mt-2">
-                        {loading
-                            ? Array.from({ length: 8 }).map((_, index) => (
-                                  <div key={index} className="flex flex-col space-y-3 mt-8">
-                                      <Skeleton className="flex h-5 w-[250px] mb-2" />
-                                      <Skeleton className="h-[160px] w-[300px] rounded-xl" />
-                                      <div className="space-y-2">
-                                          <Skeleton className="h-5 w-[250px] mb-6" />
-                                          <div className="flex justify-end me-10">
-                                              <Skeleton className="h-10 w-10 rounded-md" />
+        <Suspense>
+            <main className="flex flex-col px-4 sm:px-8 lg:px-12 xl:px-0 xl:container">
+                {loading ? (
+                    <Skeleton className="h-9 w-[200px]" />
+                ) : (
+                    <>
+                        <div className="flex gap-4">
+                            <Popover open={open} onOpenChange={setOpen}>
+                                <PopoverTrigger asChild>
+                                    <Button variant="outline" role="combobox" aria-expanded={open} className="w-[200px] justify-between">
+                                        {regionValue ? locationsForFilter.find((region) => region === regionValue) : "Search region..."}
+                                        <ChevronsUpDown className="ml-2 h-4 w-4 shrink-0 opacity-50" />
+                                    </Button>
+                                </PopoverTrigger>
+                                <PopoverContent className="w-[200px] p-0">
+                                    <Command>
+                                        <CommandInput placeholder="Search region..." />
+                                        <CommandList>
+                                            <CommandEmpty>No location found.</CommandEmpty>
+                                            <CommandGroup>
+                                                {locationsForFilter.map((region) => (
+                                                    <CommandItem key={region} value={region} onSelect={() => handleLocationSelect(region)}>
+                                                        <Check className={cn("mr-2 h-4 w-4", regionValue === region ? "opacity-100" : "opacity-0")} />
+                                                        {region}
+                                                    </CommandItem>
+                                                ))}
+                                            </CommandGroup>
+                                        </CommandList>
+                                    </Command>
+                                </PopoverContent>
+                            </Popover>
+                            <Button variant="outline" onClick={handleToggleView}>
+                                {showSelectedOnly ? "View All Bosses" : "View Selected Bosses"}
+                            </Button>
+                        </div>
+                        <section className="grid sm:grid-cols-2 lg:grid-cols-4 grid-rows-1 gap-4 mt-2">
+                            {loading
+                                ? Array.from({ length: 8 }).map((_, index) => (
+                                      <div key={index} className="flex flex-col space-y-3 mt-8">
+                                          <Skeleton className="flex h-5 w-[250px] mb-2" />
+                                          <Skeleton className="h-[160px] w-[300px] rounded-xl" />
+                                          <div className="space-y-2">
+                                              <Skeleton className="h-5 w-[250px] mb-6" />
+                                              <div className="flex justify-end me-10">
+                                                  <Skeleton className="h-10 w-10 rounded-md" />
+                                              </div>
                                           </div>
                                       </div>
-                                  </div>
-                              ))
-                            : displayedData.map((boss: any) =>
-                                  !isSelected[boss._id] ? (
-                                      <Card key={boss._id} className="flex flex-col justify-between">
-                                          <CardHeader>
-                                              <CardTitle className="text-xl font-bold">{boss.name}</CardTitle>
-                                          </CardHeader>
-                                          <CardContent className="pb-0 text-2xl font-bold">
-                                              {boss.image ? <Image className="rounded-md max-h-40 w-full" width={300} height={300} src={boss.image} alt="Picture of boss" /> : <p>No image found.</p>}
-                                          </CardContent>
-                                          <CardContent className="text-sm mt-2">
-                                              <span className="font-bold">Location:</span>
-                                              <span className="text-muted-foreground"> {boss.location}</span>
-                                          </CardContent>
-                                          <CardContent className="flex justify-end">
-                                              <Button className="hover:invert" variant="outline" size="icon" onClick={() => toggleSelected(boss._id, boss.name)}>
-                                                  <Check className="h-4 w-4" />
-                                              </Button>
-                                          </CardContent>
-                                      </Card>
-                                  ) : null
-                              )}
-                    </section>
-                </>
-            )}
-            <Toaster />
-        </main>
+                                  ))
+                                : displayedData.map((boss: any) =>
+                                      !isSelected[boss._id] ? (
+                                          <Card key={boss._id} className="flex flex-col justify-between">
+                                              <CardHeader>
+                                                  <CardTitle className="text-xl font-bold">{boss.name}</CardTitle>
+                                              </CardHeader>
+                                              <CardContent className="pb-0 text-2xl font-bold">
+                                                  {boss.image ? (
+                                                      <Image className="rounded-md max-h-40 w-full" width={300} height={300} src={boss.image} alt="Picture of boss" />
+                                                  ) : (
+                                                      <p>No image found.</p>
+                                                  )}
+                                              </CardContent>
+                                              <CardContent className="text-sm mt-2">
+                                                  <span className="font-bold">Location:</span>
+                                                  <span className="text-muted-foreground"> {boss.location}</span>
+                                              </CardContent>
+                                              <CardContent className="flex justify-end">
+                                                  <Button className="hover:invert" variant="outline" size="icon" onClick={() => toggleSelected(boss._id, boss.name)}>
+                                                      <Check className="h-4 w-4" />
+                                                  </Button>
+                                              </CardContent>
+                                          </Card>
+                                      ) : null
+                                  )}
+                        </section>
+                    </>
+                )}
+                <Toaster />
+            </main>
+        </Suspense>
     );
 }
